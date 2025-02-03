ed9b01599f4faf1e92b1adedc5bd423c478c7389
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-02-03 00:17:55 -0500

    Modified fetch for API, added icon for webpage
    Modified JS code to add API to modify the visitor count. Modified HTML code to add an icon to the webpage

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-function counter_update(){
+
+const getApiURL = "https://hfdp9i9upb.execute-api.us-east-1.amazonaws.com/Test/getCounterItem"
+
+async function counter_update(){
     var counter = document.getElementById("counter");
     var count = 0;
-    count = parseInt(counter.innerHTML);
-    count = count + 1;
+
+    try{
+        //const call_api = await fetch("https://c1b950zpkg.execute-api.us-east-1.amazonaws.com/default/visitor-counter");
+        fetch("https://c1b950zpkg.execute-api.us-east-1.amazonaws.com/default/visitor-counter");
+        //if(!call_api.ok){
+            //throw new Error("Network response error when calling Lambda API")
+        //}
+
+        const response = await fetch(`${getApiURL}?timestamp=${new Date().getTime()}`);
+        if(!response.ok){
+            throw new Error("Network response error when fetching DynamoDB API")
+        }
+        
+        //const data = response.json();
+        const data = await response.json();
+        console.log("Item data: ", data);
+        count = data.body;
+        
+        /*count = response.json();
+        console.log(count);
+        counter.innerHTML = count;*/
+    }
+    catch(error){
+        console.error("Error fetching count data");
+    }
     counter.innerHTML = count;
-    fetch("https://c1b950zpkg.execute-api.us-east-1.amazonaws.com/default/visitor-counter");
 }
 window.onload = counter_update;
